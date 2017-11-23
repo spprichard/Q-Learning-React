972360cded01d4c57e4db44a91e257676f1dc8e0
stevenprichard <steven.prichard@boldcommerce.com>
2017-11-22 19:55:00 -0600

    added input box, can enter in names now

---
+++ src/components/AddGreeter.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+
+class AddGreeter extends Component {
+    constructor(props){
+        super(props);
+        this.handleUpdate = this.handleUpdate.bind(this);
+        this.addGreeting = this.addGreeting.bind(this);
+        this.state = {
+            greetingName: ''
+        }
+    }
+
+    addGreeting(){
+        this.props.addGreeting(this.state.greetingName);
+        this.setState({
+            greetingName: ''
+        });
+    }
+
+    handleUpdate(event){
+        this.setState({
+            greetingName : event.target.value
+
+        });
+    }
+
+    render(){
+        return (
+            <div className = "AddGreeter"> 
+                <p className="Title">Enter your name:</p>
+                <input type="text" onChange={this.handleUpdate}/>
+                &nbsp;&nbsp;
+                <button onClick={this.addGreeting}>Add</button>
+            </div>
+        );
+    }
+}
+
+export default AddGreeter;
--- src/components/HelloWorldList.js
+++ src/components/HelloWorldList.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import HelloWorld from "./HelloWorld"
+import HelloWorld from "./HelloWorld";
+import AddGreeter from "./AddGreeter";
 
 class HelloWorldList extends Component{
     constructor(props){
         super(props);
+        this.addGreeting = this.addGreeting.bind(this);
         this.state = {
-            greetings: ["James","Tess", "Steven", "Carlos"]
+            greetings: []
         }
+    }
+
+    addGreeting(newName){
+        this.setState({
+            greetings: [...this.state.greetings, newName]
+        });
     }
 
     renderGreetings(){
@@ -19,6 +27,7 @@ class HelloWorldList extends Component{
     render(){
         return(
         <div className="HelloWorldList">
+            <AddGreeter addGreeting={this.addGreeting}/>
             {this.renderGreetings()}
         </div>
         );
