0813fb8b2c15e8b817ec2d69aad684504a097acb
stevenprichard <steven.prichard@boldcommerce.com>
2017-11-22 19:25:30 -0600

    added renderGreeting to HelloWorldList component

--- src/components/HelloWorldList.js
+++ src/components/HelloWorldList.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import HelloWorld from "./HelloWorld"
 
 class HelloWorldList extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            greetings: ["James","Tess", "Steven", "Carlos"]
+        }
+    }
+
+    renderGreetings(){
+        return this.state.greetings.map(name => (
+            <HelloWorld key={name} name={name}/>
+        ));
+    }
+
+
     render(){
         return(
         <div className="HelloWorldList">
-            <HelloWorld name="Tess"/>
-            <HelloWorld name="Steven"/>
+            {this.renderGreetings()}
         </div>
         );
     }
