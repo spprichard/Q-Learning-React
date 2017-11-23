9f28b967c9a1c42ffe4ee4428007eb37c46406e6
stevenprichard <steven.prichard@boldcommerce.com>
2017-11-23 17:40:34 -0600

    Added things to header message

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Q-Learning Agent</h1>
+          <h1 className="App-title">Q-Learning Agent & Stuff</h1>
         </header>
           <HelloWorldList/>
       </div>
