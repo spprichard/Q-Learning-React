d00c8d1967e2c227a1ce3f265e9ab600bfb85307
stevenprichard <steven.prichard@boldcommerce.com>
2017-11-24 13:19:06 -0600

    refactored test

--- src/App.test.js
+++ src/App.test.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import { ConnectedRouter } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
-import App from '/containers/App/App';
-// import App from ''
+import App from './containers/App/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   const thing = (
   <Provider store={store}>
     <ConnectedRouter history={history}>
-        <App/>
+        <div>
+            <App/>
+        </div>
     </ConnectedRouter>
   </Provider>
   )
--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
 import store, { history } from './store'
 import App from './containers/App/App'
-
 import './index.css'
-import registerServiceWorker from './registerServiceWorker';
 
 const  target = document.querySelector('#root')
 
