47cd3ddaabaf63ea84620be2dcac214f658964ef
stevenprichard <steven.prichard@boldcommerce.com>
2017-11-24 13:10:49 -0600

    refactored test

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import { ConnectedRouter } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
-import App from './containers/App/App';
-import Nav from './components/Nav/Nav';
+import App from '../containers/App/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
